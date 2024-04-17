96b957054e9db1c164986c95f9a6e7cb9d8bb2ad
Egor Pilipenko <ctapchuk@gmail.com>
2024-04-17 15:20:47 +0700

    marble: Try to detect cases when need to unload goodix_core touch module Check if properly goodix_core module is loaded by init. MIUI/HOS init loads unusable module with broken touch, but on some custom roms goodix_core loads with working touch (and in this case we cannot rmmod module otherwise system will crash).
    Signed-off-by: Egor Pilipenko <ctapchuk@gmail.com>

--- recovery/root/system/bin/beforemodules.sh
+++ recovery/root/system/bin/beforemodules.sh
@@ -10,10 +10,22 @@ modules=/vendor/lib/modules;
 mkdir -p $modules/1.1;
 if strings /dev/block/bootdevice/by-name/xbl_config${slot} | grep -q 'led_blink'; then
 	echo "I:modules_fix: Use kernel modules for HyperOS firmware!" >> $LOGF;
-	mount $modules/hos1 $modules/1.1 --bind
+	mount $modules/hos1 $modules/1.1 --bind;
 else
 	echo "I:modules_fix: Use kernel modules for MIUI14 firmware!" >> $LOGF;
-	mount $modules/miui14 $modules/1.1 --bind
+	mount $modules/miui14 $modules/1.1 --bind;
+fi
+
+# Workaround for goodix_core touch module
+cat /proc/modules | grep goodix_core > /dev/null;
+status=$?;
+if [ $status -eq 0 -a ! -d "/sys/devices/platform/goodix_ts.0" ]; then
+	rmmod goodix_core;
+	if [ $? -eq 0 ]; then
+		echo "I:modules_fix: goodix_core unloaded successfully!" >> $LOGF;
+	else
+		echo "E:modules_fix: Cannot unload goodix_core!" >> $LOGF;
+	fi
 fi
 
 exit 0
