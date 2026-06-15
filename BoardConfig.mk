e9f3dd7f1a3ed435f06b62cdb8d4d572acbfa2e1
Egor Pilipenko <ctapchuk@gmail.com>
2026-06-15 23:02:03 +0700

    marble: WLAN bring up
    Signed-off-by: Egor Pilipenko <ctapchuk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdebuggerd_client.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko goodix_core.ko"
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko goodix_core.ko qca_cld3_qca6490.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_NO_SCREEN_BLANK := true
 #TW_ALLOW_REWRITE_SUPER_METADATA := true
